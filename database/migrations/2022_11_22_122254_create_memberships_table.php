cd7a4867126f11dc1cdab7b68a9dfede5e2ce37a
Haviz Indra Maulana <haviz.indra@codr.id>
2022-11-22 19:42:48 +0700

    Setup MIgration for Master Membership

---
+++ database/migrations/2022_11_22_122254_create_memberships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->integer('duration_in_month')->default(1);
+            $table->double('price', 13, 2);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memberships');
+    }
+};
---
+++ database/seeders/MembershipSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Membership;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class MembershipSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'Paket Basic',
+                'duration_in_month' => 1,
+                'price' => 50000
+            ],
+            [
+                'name' => 'Paket Middle',
+                'duration_in_month' => 3,
+                'price' => 130000
+            ],
+            [
+                'name' => 'Paket Advance',
+                'duration_in_month' => 6,
+                'price' => 250000
+            ]
+        ];
+
+        foreach($data as $val) {
+            Membership::create($val);
+        }
+    }
+}
